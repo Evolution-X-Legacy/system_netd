92dcadc61270cf803aa8cc0d237aa1c3f60c602e
Pierre-Hugues Husson <phh@phh.me>
2019-07-29 18:09:12 +0200

    Don't fail on FTP conntracking failing
    The issue has been seen on some Samsung devices.
    See https://github.com/phhusson/treble_experimentations/issues/425
    
    Thanks @zamrih for pin-pointing the issue and validating fix
    
    Change-Id: I3d9c865eb5a4b421f9983210c2ceae62b4906234

--- server/TetherController.cpp
+++ server/TetherController.cpp
@@ -637,13 +637,17 @@ int TetherController::setForwardRules(bool add, const char *intIface, const char
         return -1;
     }
 
+    std::vector<std::string> v4Ftp = {
+            "*raw",
+            StringPrintf("%s %s -p tcp --dport 21 -i %s -j CT --helper ftp",
+                            op, LOCAL_RAW_PREROUTING, intIface),
+            "COMMIT",
+    };
+    if(iptablesRestoreFunction(V4, Join(v4Ftp, '\n'), nullptr) == -1) {
+            ALOGE("Failed adding iptables CT target on FTP.");
+    }
+
     std::vector<std::string> v4 = {
-#ifndef OMIT_NETD_TETHER_FTP_HELPER
-        "*raw",
-        StringPrintf("%s %s -p tcp --dport 21 -i %s -j CT --helper ftp",
-                     op, LOCAL_RAW_PREROUTING, intIface),
-        "COMMIT",
-#endif
         "*filter",
         StringPrintf("%s %s -i %s -o %s -m state --state ESTABLISHED,RELATED -g %s",
                      op, LOCAL_FORWARD, extIface, intIface, LOCAL_TETHER_COUNTERS_CHAIN),
